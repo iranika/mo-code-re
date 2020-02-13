9a83cdea1225603a017be54c679a368099617974
iranika <7370486+iranika@users.noreply.github.com>
2020-02-13 11:57:44 +0900

    Add lang option menu

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -8,6 +8,9 @@ import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import koma4 from './data/koma4.json';
 
@@ -25,17 +28,37 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+//TODO: optionsは親要素で管理する
+const options = [
+  'Japanese',
+  'English',
+  'Chinese',
+];
+
 export default function ButtonAppBar() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const title = "みちくさびゅあー"
+  const [anchorE2, setAnchorE2] = React.useState<null | HTMLElement>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
+  };
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorE2(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (event: React.MouseEvent<HTMLElement>, index: number) => {
+    setSelectedIndex(index);
+    setAnchorE2(null);
+  };
+  const handleClose2 = () => {
+    setAnchorE2(null);
   };
   return (
     <div className={classes.root}>
@@ -44,6 +67,45 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             {title}
           </Typography>
+          <List component="nav" aria-label="Device settings">
+            <ListItem
+              button
+              aria-haspopup="true"
+              aria-controls="lock-menu"
+              aria-label="LANG"
+              onClick={handleClickListItem}
+            >
+              <ListItemText primary={options[selectedIndex]} />
+            </ListItem>
+          </List>
+          <Menu
+            id="lock-menu"
+            anchorEl={anchorE2}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorE2)}
+            onClose={handleClose2}
+          >
+            {options.map((option, index) => (
+              <MenuItem
+                key={option}
+                disabled={index === selectedIndex}
+                selected={index === selectedIndex}
+                onClick={event => handleMenuItemClick(event, index)}
+              >
+                {option}
+              </MenuItem>
+            ))}
+          </Menu>
+          
+
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleMenu}>
             <MenuIcon />
           </IconButton>
